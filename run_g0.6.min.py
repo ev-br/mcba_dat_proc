35783b589a4839a86bd00c0abecdae051daa1f6a
Evgeni Burovski <evgeni@burovski.me>
2015-05-13 21:39:18 +0100

    ENH: add P_inf(P_0) runfile and nb
    Targeting the inflection point

---
+++ run_g0.6.min.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+from itertools import product
+
+from mcba.models.impurity import Par, SingleImpurity, gamma, initial_q
+from mcba.walker import Walker
+from mcba.helpers import copydict
+from mcba.walker_factory import WalkerFactory
+
+
+
+def main():
+
+    mc_dict = {    "num_sweeps"      : np.Inf,
+              "steps_per_sweep" : 100,
+              "therm_sweeps"    : 0,
+              "checkp_sweeps"   : 100,
+              "printout_sweeps" : 100,
+              "seed"            : 42,
+              "verbose_logg"    : True,
+              "threshold"       : 0.995, 
+              "store_roots"     : False,
+            }
+
+    V = 0.2
+    N0 = 45
+    N_f = [1, 3]
+#    mqs = [6, 7, 8, 9, 10, 11, 12, 13]  # @ N = 15
+    mqs = [21, 22, 23, 24, 25, 26, 27]   # @ N = 45
+
+    base_path="../data/g1.2"
+    tasks = []
+    for f in N_f:
+        N = N0 * f
+        for mq in mqs:
+            par = Par(N=N, L=3*N, V=V, m_q=mq*f)
+            db_fname = os.path.join(base_path,
+                    "N{0}V0.2mq{1}.sqlite".format(par.N, par.m_q))
+            dct = {"model": SingleImpurity(par), 
+                   "db_fname": db_fname}
+            tasks.append( copydict(mc_dict, dct) )
+
+#    for task in tasks:
+#        p = task['model'].par
+#        print task, p
+#        print 'GAMMA: ', gamma(p), 'q = ', initial_q(p), '\n'
+#    print len(tasks)
+        
+    factory = WalkerFactory(tasks, 5, freq=1, verbose=True)
+    factory.start()
+
+#######################
+if __name__ == "__main__":
+    main()
+
